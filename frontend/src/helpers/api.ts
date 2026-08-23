9a373624da0b544674a138f5a7d8d0d04ae95557
OtavioLazzarotto <otavio2026lazzarotto@gmail.com>
2026-08-22 22:28:52 -0300

    testing deploy

--- frontend/src/helpers/api.ts
+++ frontend/src/helpers/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "api-waste-control-production.up.railway.app",
+    "https://api-waste-control.railway.internal",
 });
 
 // Injeta o token em todas as requisições
